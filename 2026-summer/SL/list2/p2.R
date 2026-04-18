ab00bd81771985ace0d3263c755b8c45a5783bab
pszen.beton <adpszeniczny@gmail.com>
2026-04-18 18:30:49 +0200

    working on SL list, created folder for Master's Thesis Seminars

--- 2026-summer/SL/list2/p2.R
+++ 2026-summer/SL/list2/p2.R
@@ -8,18 +8,54 @@ C_10 = matrix(0.8, nrow = 10, ncol = 10) + diag(0.2, 10)
 
 Cov_2 = I_30 %x% C_10
 
-covs = c(I_300, Cov_2)
+covs = list(I_300, Cov_2)
+cov_lab = c('I_300', 'Cov_2')
+
+X = rmvnorm(1, mean=rep(0,10))
+
+LW_optimal_lambda <- function(X){
+  Yks = apply(X, 1, function(row){
+    cov(row)
+  })
+  return(Yks)
+}
+
+LW_optimal_lambda(X)
 
 library(mvtnorm)
 
 matrices <- lapply(1:100, function(iter){
-  lapply(covs, function(c){
+  lapply(1:2, function(i){
+    cov_mat = covs[[i]]
     mu = rep(0, 300)
-    X = rmvnorm(100, mean=mu, sigma = c)
+    X = rmvnorm(100, mean=mu, sigma = cov_mat)
     S = cov(X)
     
-    risk = sum((c - S)^2)
+    lLW = linearShrinkLWEst(X)
+    nlLW = nlShrinkLWEst(X)
     
-    list(risk = risk)
+    risk_S = sum((cov_mat - S)^2)
+    risk_lLW =sum((cov_mat - lLW)^2)
+    risk_nlLW = sum((cov_mat - nlLW)^2)
+    
+    list(label = cov_lab[i],
+      risk_S = risk_S,
+      risk_lLW = risk_lLW,
+      risk_nlLW = risk_nlLW)
   })
 })
+
+matrices_table = rbindlist(unlist(matrices, F, F))
+
+boxplots = lapply(c('risk_S', 'risk_lLW', 'risk_nlLW'), function(risk){
+  ggplot(data=matrices_table, aes(y= .data[[risk]])) +
+    geom_boxplot(fill='steelblue', col='black') +
+    facet_grid(~label) +
+    theme_bw() +
+    ylim(0, 1000)
+})
+
+boxplots[[1]] + boxplots[[2]] + boxplots[[3]]
+
+
+
